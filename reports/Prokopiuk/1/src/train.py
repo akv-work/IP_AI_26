0af7fb408ee1bd073b128948b158899a47b8ab42
hansshipher <rgwgff@gmail.com>
2026-09-06 15:26:37 +0300

    Add graphics stuff

--- reports/Prokopiuk/1/src/train.py
+++ reports/Prokopiuk/1/src/train.py
@@ -3,6 +3,8 @@ import torch
 def train_loop(dataloader, model, loss_fn, optimizer, device):
     model.train()
     size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    train_loss = 0.0
 
     for batch, (X, y) in enumerate(dataloader):
         X, y = X.to(device), y.to(device)
@@ -14,10 +16,15 @@ def train_loop(dataloader, model, loss_fn, optimizer, device):
         loss.backward()
         optimizer.step()
 
+        train_loss += loss.item()
+
         if batch % 100 == 0:
             loss_val = loss.item()
             current = batch * dataloader.batch_size + len(X)
             print(f"loss: {loss_val:>7f}  [{current:>5d}/{size:>5d}]")
+
+    avg_train_loss = train_loss / num_batches
+    return avg_train_loss
 
 def test_loop(dataloader, model, loss_fn, device):
     model.eval()
@@ -37,3 +44,5 @@ def test_loop(dataloader, model, loss_fn, device):
     print(
         f"Test Error:\n Accuracy: {(100*correct):>0.01f}%, Avg loss: {test_loss:>8f}\n"
     )
+
+    return test_loss, correct*100
